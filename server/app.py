a1ecbe02082e129fd922c5585e5b5851c5fd25dc
faith kaburu <faith.kaburu@student.moringaschool.com>
2023-09-30 12:37:50 +0300

    initial migration

--- server/app.py
+++ server/app.py
@@ -20,19 +20,53 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+
+    response = make_response(
+        jsonify(bakeries_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        bakery_dict,
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_good_dict = [baked_good.to_dict() for baked_good in baked_goods]
+
+    response = make_response(
+        baked_good_dict,
+        200,
+        {"Content-Type": "application/json"}
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    baked_good_dict = baked_good.to_dict()
+    
+    response = make_response(
+        baked_good_dict,
+        200,
+        {"Content-Type": "application/json"}
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
